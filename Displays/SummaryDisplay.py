2c1a196a1ebfc70ae153df3e379756e1fbc9f9b7
Mosa Abualkam <m.abualkam@cs.huji.ac.il>
2024-09-10 20:28:40 +0300

    summary updated

--- Displays/SummaryDisplay.py
+++ Displays/SummaryDisplay.py
@@ -1,4 +1,7 @@
 import time
+
+import numpy as np
+
 from Displays.Display import Display
 
 
@@ -6,10 +9,9 @@ class SummaryDisplay(Display):
     def __init__(self):
         super(SummaryDisplay, self).__init__()
         self.winner = []
-        self.highest_tile = []
         self.game_durations = []
         self.game_start_time = None
-
+        self.num_steps= []
     def initialize(self, initial_state):
         self.game_start_time = time.time()
 
@@ -17,17 +19,13 @@ class SummaryDisplay(Display):
         if new_state.done:
             game_end_time = time.time()
             game_duration = game_end_time - self.game_start_time
-            print("winner: %s\ngame_duration: %s" % (new_state.winner, game_duration))
+            # print("winner: %s\ngame_duration: %s" % (new_state.winner, game_duration))
             self.winner.append(new_state.winner)
             # self.highest_tile.append(new_state.board.max())
             self.game_durations.append(game_duration)
-
+            b = new_state.board
+            self.num_steps.append(np.count_nonzero(b)//2)
 
 
     def print_stats(self):
-        # win_rate = len(list(filter(lambda x: x >= 2048, self.highest_tile))) / len(self.highest_tile)
-        print("=" * 30)
-        print("winner: %s" % self.winner)
-        # print("highest tile: %s" % self.highest_tile)
-        print("game_durations: %s" % self.game_durations)
-        print("win rate: %s" % 0)
+        return self.winner, self.game_durations,self.num_steps
